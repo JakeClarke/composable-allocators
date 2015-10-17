cdc060f7b2cf2801551d1ab5d99e2e0a037482a7
Jake Clarke <clarke.jake@gmail.com>
2015-10-18 00:37:24 +0100

    Added global allocator

---
+++ globalalloc.h
@@ -0,0 +1,35 @@
+#ifndef _H_GLOBALALLOC
+#define _H_GLOBALALLOC
+
+#include "alloc-common.h"
+
+template <class t>
+class GlobalAlloc
+{
+public:
+	Block allocate(size_t size) {
+		return inst.allocate(size);
+	}
+
+	void deallocate(Block blk) {
+		inst.deallocate(blk);
+	}
+
+	bool reallocate(Block &blk, size_t delta) {
+		return inst.reallocate(blk, delta);
+	}
+
+	bool owns(Block blk) {
+		return inst.owns(blk);
+	}
+
+	static constexpr size_t goodSize(size_t size) {
+		return t::goodSize(size);
+	}
+private:
+	static t inst;
+};
+
+template <class tp> tp GlobalAlloc<tp>::inst;
+
+#endif
